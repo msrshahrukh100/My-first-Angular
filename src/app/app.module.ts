70a7ea92b26d75715eb466f3257c763fc44bad6f
Shahrukh <msr.concordfly@gmail.com>
2018-02-04 09:51:45 +0530

    The initial setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { WriteComponent } from './write/write.component';
+import { MarkdownModule } from 'angular2-markdown';
+import { DisplayComponent } from './display/display.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WriteComponent,
+    DisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    HttpModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
